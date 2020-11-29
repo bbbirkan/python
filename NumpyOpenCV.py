6a873d011c5075f3694bf6e6160bb0b1d10cfd1d
Birkan Kalyon <bbbirkan@hotmail.com>
2020-11-29 13:35:10 -0500

    Opencv 3 gun

--- NumpyOpenCV.py
+++ NumpyOpenCV.py
@@ -1,2 +1,27 @@
-benim listem=[1,2,4]
+import numpy as np
+import matplotlib.pyplot as plt
+import keras
+from PIL import Image
+
+
+
+resim =Image.open('/Users/birkankalyon/Desktop/RUZ.jpg')
+print(type(resim))
+resim_dizi=np.asarray(resim)
+print(type(resim_dizi))
+#resimi aldik numpyla dizi formatinda cevirdik
+print(resim_dizi.shape)
+"""
+<class 'PIL.JpegImagePlugin.JpegImageFile'>
+<class 'numpy.ndarray'>
+(5196, 7776, 3)
+"""
+# %matplotlib inline
+# plt.imshow(resim_dizi) birlikte jupiterde kullanilir
+plt.imshow(resim_dizi)
+
+plt.savefig('resim_dizi')
+#ekrana cikmadi herneyse
+
+
 
