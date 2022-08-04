6fc41904c360e0d91f854cb3a34c2435a36eb5f9
bbbirkan <58374229+bbbirkan@users.noreply.github.com>
2022-08-04 11:50:48 -0400

    Created ai folder

---
+++ AI/DALL·E.py
@@ -0,0 +1,34 @@
+'''
+
+LATEST MODEL	DESCRIPTION	MAX REQUEST	TRAINING DATA
+text-davinci-002	Most capable GPT-3 model. Can do any task the other models can do, often with less context. In addition to responding to prompts, also supports inserting completions within text.	4,000 tokens	Up to Jun 2021
+text-curie-001	Very capable, but faster and lower cost than Davinci.	2,048 tokens	Up to Oct 2019
+text-babbage-001	Capable of straightforward tasks, very fast, and lower cost.	2,048 tokens	Up to Oct 2019
+text-ada-001	Capable of very simple tasks, usually the fastest model in the GPT-3 series, and lowest cost.	2,048 tokens	Up to Oct 2019
+'''
+
+
+import os,json
+import openai
+
+os.environ["key"]="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"
+
+
+
+
+openai.api_key = os.getenv("key")
+
+response = openai.Completion.create(
+  model="text-davinci-002",
+  prompt="corect grammer mistake this sentence.'Hey how ist going' ",
+  temperature=0,
+  max_tokens=100,
+  top_p=1,
+  frequency_penalty=0.0,
+  presence_penalty=0.0,
+
+)
+
+result=response["choices"]
+data=""
+print(result)
