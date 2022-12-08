b6f82f5f685bbc14462ab2c038ae0390af95f937
bbbirkan <58374229+bbbirkan@users.noreply.github.com>
2022-12-08 00:27:48 -0500

    Whatsapp automatic message with web browser 2

--- whatspass/whatsapp-messages.py
+++ whatspass/whatsapp-messages.py
@@ -5,42 +5,19 @@ import emoji
 import random
 import time
 
-def send_whatsapp_message(phoneNumber, msg, hour, minute):
-    try:
-        pywhatkit.sendwhatmsg(phoneNumber, msg, hour, minute)
-        time.sleep(10)
-        pyautogui.click()
-        time.sleep(1)
-        Controller().press(Key.enter)
-        Controller().release(Key.enter)
-        time.sleep(1)
-        print("Message sent!")
-    except Exception as e:
-        print(str(e))
-
 theMessage = random.choice(['Hi, How are you?',
              'How is your day?',
              emoji.emojize('I love you :sparkling_heart:'),
-             emoji.emojize(':red_heart:", variant="emoji_type'),
+             emoji.emojize(':red_heart:"'),
              emoji.emojize(':beating_heart:'),
              emoji.emojize(':smiling_face_with_heart-eyes:')])
-
 print(theMessage)
+
 phoneNumber = "+16092024224"
-numberOfMessages = 4
-startingHour =23
-endingHour =24
-hour =23 #random.randrange(startingHour, endingHour)
 
-minute =45
-
-print(hour,minute)
-
-seconds = time.time()
-result = time.localtime(seconds)
-print("result:", result)
-print("\nyear:", result.tm_year)
+result = time.localtime(time.time())
 print("tm_hour:", result.tm_hour)
+print("tm_min",result.tm_min+1)
 
 
-pywhatkit.sendwhatmsg(phoneNumber, theMessage, hour, minute)
+pywhatkit.sendwhatmsg(phoneNumber, theMessage, result.tm_hour,result.tm_min+1)
